8ff85f6e95d55c315addbd32d29ca291fdf9a715
Juan Pablo <100604780+MrCuacky@users.noreply.github.com>
2023-01-13 14:38:01 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi Práctica con SANDBOX!</h1>
+<h2>Juan Pablo Contreras Vasquez<h2>
+<div>
+  Universidad Tecnologica de Durango
+  <a href="https://github.com/MrCuacky/prometeo.git" target="_blank" rel="noopener noreferrer">Mi GitHub</a>.
+</div>
+`;
